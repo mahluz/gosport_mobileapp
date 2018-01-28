3e21276bb33836ea3b3c09f2d2f25a98d08b55a5
LailatulKhusna <lailatulkhusna38@gmail.com>
2018-01-28 22:53:57 +0700

    update

--- src/pages/list-master/list-master.ts
+++ src/pages/list-master/list-master.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, ModalController, NavController } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
-
+import { HttpClient } from '@angular/common/http';
+import { Storage } from '@ionic/storage';
 import { Item } from '../../models/item';
 import { Items } from '../../providers/providers';
 import { AuthServiceProvider } from '../../providers/auth-service/auth-service';
@@ -14,9 +15,14 @@ import { AuthServiceProvider } from '../../providers/auth-service/auth-service';
 export class ListMasterPage {
   services:any;
 
-  constructor(public navCtrl: NavController, public items: Items, public modalCtrl: ModalController, public auth:AuthServiceProvider,public storage:Storage) {
+  constructor(
+    public navCtrl: NavController, 
+    public items: Items, 
+    public modalCtrl: ModalController, 
+    public auth:AuthServiceProvider,
+    public storage:Storage,
+    public http:HttpClient) {
     // this.currentItems = this.items.query();
-    this.auth.getService();
     // console.log("problem",this.problems);
   }
 
@@ -24,11 +30,15 @@ export class ListMasterPage {
    * The view loaded, let's query our items for the list
    */
   ionViewDidLoad() {
-    this.storage.get('services').then((data)=>{
-      console.log("problem storage",data);
-      this.services = data['result'];
-      console.log("problem variable",this.services[0]);  
+    this.storage.get('token').then((data)=>{
+      let access = {token:data};
+      this.http.post('http://localhost/gosport_server/api/services',access).subscribe(data=>{
+        console.log("post",data);
+        this.services=data["result"];
+      });
+
     });
+    
   }
 
   /**
--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -5,6 +5,7 @@ import { Storage } from '@ionic/storage';
 import { Item } from '../../models/item';
 import { Items } from '../../providers/providers';
 import { AuthServiceProvider } from '../../providers/auth-service/auth-service';
+import { AlertController, LoadingController, Loading } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -20,7 +21,8 @@ export class SearchPage {
   	public items: Items,
   	public http:HttpClient,
   	public storage:Storage,
-  	public auth:AuthServiceProvider) { 
+  	public auth:AuthServiceProvider,
+    public alertCtrl:AlertController) { 
   	// this.user = "ulala";
   }
 
@@ -49,5 +51,54 @@ export class SearchPage {
   	});
 
   }
+
+  refresh(){
+    this.storage.get('token').then(data=>{
+      let access = {
+        token:data,
+        user_id:this.auth.currentUser["id"]
+      };
+
+      this.http.post('http://localhost/gosport_server/api/getRequest',access).subscribe(result=>{
+        this.requests = result["result"];
+        console.log('hasilnya',result["result"]);
+        console.log('requestnya',this.requests);
+      });
+    });
+  }
+
+  cancelOrder(order_id){
+    console.log(order_id);
+    this.storage.get('token').then(data=>{
+      let access = {token:data,order_id:order_id};
+      this.http.post('http://localhost/gosport_server/api/cancelOrder',access).subscribe(result=>{
+        this.showAlert('Attention','Your order has been canceled');
+        this.refresh();
+      });
+    });
+  }
+
+  finishOrder(order_id){
+    console.log(order_id);
+
+    this.storage.get('token').then(data=>{
+      let access = {token:data,order_id:order_id};
+      this.http.post('http://localhost/gosport_server/api/finishOrder',access).subscribe(result=>{
+        console.log(result);
+        this.showAlert('Sucessfully','Thanks for using our Service');
+        this.refresh();
+      });
+    });
+  }
+
+  showAlert(title,message) {
+    let alert = this.alertCtrl.create({
+      title: title,
+      subTitle: message,
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
 
 }
